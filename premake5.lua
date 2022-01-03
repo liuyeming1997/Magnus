8a33703eeee4717afe8aba34639d790310bb1f5e
Yeming Liu <yemingl@student.unimelb.edu.au>
2022-01-03 15:27:11 +0800

    add premake5

---
+++ premake5.lua
@@ -0,0 +1,105 @@
+workspace "Magnus"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Magnus"
+	location "Magnus"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0"
+
+		defines
+		{
+			"MG_PLATFORM_WINDOWS",
+			"MG_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "MG_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "MG_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "MG_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Magnus/vendor/spdlog/include",
+		"Magnus/src"
+	}
+
+	links
+	{
+		"Magnus"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0"
+
+		defines
+		{
+			"MG_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "MG_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "MG_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "MG_DIST"
+		optimize "On"
